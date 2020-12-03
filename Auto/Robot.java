792d23e9deec7b868c33bcb40486905695bba721
Silas Behnke <56367282+SilasBehnke@users.noreply.github.com>
2020-12-03 15:19:48 -0500

    Update Robot.java
    Taking the movement class from Movement.java and initializing it here rather than another file. Prevents compiler errors

--- Auto/Robot.java
+++ Auto/Robot.java
@@ -1,30 +1,119 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
-
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+//declaring the Movement class
+class Movement {
+    public enum Directions {
+        Right,
+        Left,
+        Forward,
+        Backwards;
+        public Directions InverseDirection() {
+            switch (this) {
+                case Forward:
+                    return Directions.Backwards;
+                case Backwards:
+                    return Directions.Forward;
+                case Left:
+                    return Directions.Right;
+                default:
+                    return Directions.Left;
+            }
+        }
+    }
+
+    private int Distance;
+    private int Correction;
+    private double distanceTraveled = 0; //sets the distance traveled to 0
+
+    private double LastDistance = 0;
+
+    private double MaxPower;
+
+    private double Slope;
+
+    private double MinPower = 0.15;
+
+    private Directions Direction;
+    Movement(int Distance, int Correction, double MaxPower, Directions Direction) {
+        this.Distance = Distance;
+        this.Correction = Correction;
+        this.MaxPower = MaxPower;
+        this.Direction = Direction;
+    }
+
+    Movement(int Distance, int Correction, double MaxPower) {
+        this.Distance = Distance;
+        this.Correction = Correction;
+        this.MaxPower = MaxPower;
+    }
+
+    int GetTotalDistance() {
+        return Distance + Correction;
+    }
+
+    void setPowerSign(int sign) {
+        this.MaxPower *= sign;
+        this.MinPower *= sign;
+    }
+    Directions getDirection() {
+        return Direction;
+    }
+
+    void setStartDistance(double StartDistance) {
+        double totalNeededToTravel = StartDistance - this.GetTotalDistance();
+        this.LastDistance = StartDistance;
+        double TotalNeededCubed = totalNeededToTravel * totalNeededToTravel * totalNeededToTravel;
+        this.Slope = -MaxPower / TotalNeededCubed; //gets the slope of the graph that is needed to make y = 0 when totalNeeded to travel is x
+
+    }
+
+    void UpdateDistanceTraveled(double NewDistance) {
+        distanceTraveled += LastDistance - NewDistance;
+
+        LastDistance = NewDistance;
+    }
+
+    double CalculatePower() {
+        double distanceCubed = distanceTraveled * distanceTraveled * distanceTraveled;
+        double power = (Slope * distanceCubed) + MaxPower; // the power is the x value in that position
+
+        if (power > MaxPower) power = MaxPower;
+        else if (power < -MaxPower) power = -MaxPower;
+
+        else if (power < MinPower && power > 0) power = MinPower; //if the power is less than the min power level just set the power to the minpower level
+        else if (power > MinPower && power < 0) power = -MinPower;
+
+        return power;
+    }
+
+    void SetDirection (Directions Direction) {
+        this.Direction = Direction;
+    }
+}
+
+
+
 public abstract class Robot {
     final private double MaxPwr = 0.75;
     final private double NormPower = 0.40; //The normal power to give to motors to drive
     final private double MinPower = 0.20; //slowest it should do
+    Movement MoveToZone = new Movement(7,8,NormPower);
 
-    Movement MoveToZone = new Movement(7, 8, NormPower);
 
 
-    Movement.Directions DirectionToTape;
-    Movement.Directions DirectionToScan;
     //declare motors
     private DcMotor FLM = null;
     private DcMotor FRM = null;
