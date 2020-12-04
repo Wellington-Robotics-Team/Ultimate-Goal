2f4a280bfeb13869781c954fb5ea352f909fcdfa
Silas Behnke <56367282+SilasBehnke@users.noreply.github.com>
2020-12-03 21:32:03 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAPark.java
@@ -0,0 +1,60 @@
+//Randomization A 
+//Move Wobble
+//Park on Launch Line
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//This auto was made by SiB on 12/2/2020
+
+@Autonomous(name = "RightAPark", group = "Red")
+@Disabled
+public class RightAPark extends LinearOpMode {
+    public class JankBot extends Robot {
+        @Override
+        public boolean AllowedToMove() {
+            return opModeIsActive() && !isStopRequested();
+        }
+        JankBot(boolean BlueSide) {
+            super(BlueSide);
+        }
+
+        @Override
+        public void AddToTelemetry(String Tag, String message) {
+            telemetry.addData(Tag, message);
+        }
+
+        @Override
+        public void UpdateTelemetry() {
+            telemetry.update();
+        }
+
+        @Override
+        public void Sleep(int Time) {
+            sleep(Time);
+        }
+    }
+
+    private JankBot Bot = new JankBot(true);
+
+    public void runOpMode() //when you press init
+    {
+        Bot.Init(hardwareMap);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+        telemetry.update();
+
+        waitForStart(); //waits for the start button
+
+        //Move to bricks
+        Bot.RushB(Bot.MoveToZone);
+        Bot.StopRobot();
+        stop();
+
+
+    }
+}
